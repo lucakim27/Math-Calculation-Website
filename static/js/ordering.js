c67eb92e3385a4894d7feff3d518ba0c2693910f
Heesu Kim <63194988+lucakim27@users.noreply.github.com>
2021-01-28 17:01:57 +0900

    Create ordering.js

---
+++ static/js/ordering.js
@@ -0,0 +1,26 @@
+function getOrdering() {
+
+    // getting an input from user
+    var numbers = document.getElementById('idname').value;
+    var numbers = numbers + ",";
+
+    // putting the each number inputs into a list
+    var array_for_numbers = [];
+    var each_number = ""
+    for (var i=0; i<numbers.length; i++) {
+        if (numbers[i] != ",") {
+            each_number += numbers[i];
+        }
+        else if (numbers[i] == ",") {
+
+            // eval is to evaluate an argument, push is to append values to the list
+            array_for_numbers.push(eval(each_number));
+            each_number = "";
+        }
+    }
+
+
+    array_for_numbers.sort(function(a, b){return a-b});
+    // showing the output
+    document.getElementById("demo").innerHTML = array_for_numbers;
+}
